810b129c8b6d1dc723e017173c425f64ade81a83
jsardello <jackson.sardello@gmail.com>
2024-01-19 15:06:57 -0600

    Add Dockerfile

--- src/main/java/org/example/springbackend/controllers/PushRequestController.java
+++ src/main/java/org/example/springbackend/controllers/PushRequestController.java
@@ -1,7 +1,6 @@
 package org.example.springbackend.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
