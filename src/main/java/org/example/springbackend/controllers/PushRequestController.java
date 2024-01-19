3c9f0f13268d6268c4e004763fbd1db17718c760
jsardello <jackson.sardello@gmail.com>
2024-01-19 14:40:22 -0600

    initial commit

---
+++ src/main/java/org/example/springbackend/controllers/PushRequestController.java
@@ -0,0 +1,14 @@
+package org.example.springbackend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PushRequestController {
+
+    @GetMapping("/paymentRequest")
+    public void paymentRequest() {
+        System.out.println("Payment request received");
+    }
+}
